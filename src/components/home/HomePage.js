20c1a30a18c220c19983d934da6133370037eb56
Douglas Silvio Correa <me@douglascorrea.io>
2016-06-04 16:28:14 -0300

    Home as function

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-class HomePage extends React.Component {
-  render() {
-    return (
-      <div className="jumbotron">
-        <h1>Pluralsiasdasdght Administration</h1>
-        <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
-        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
-      </div>
-    );
-  }
+const HomePage = () => {
+  return (
+    <div className="jumbotron">
+      <h1>Plural Administration</h1>
+      <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+      <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+    </div>
+  );
 }
 
 export default HomePage;
