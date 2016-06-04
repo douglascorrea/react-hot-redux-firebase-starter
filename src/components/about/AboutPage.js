cdf728e63caf4c417873081c9ba0c3a33e848511
Douglas Silvio Correa <me@douglascorrea.io>
2016-06-04 16:27:14 -0300

    About to presentacional component (function))

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-class AboutPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>About</h1>
-        <p>This applicaationasdas uses React, Redux, React Router and a variety of other helpful libraries.</p>
-        <Link to="/layout" activeClassName="active">Go to Layout</Link>
-      </div>
-    );
-  }
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This applitation uses React, Redux, React Router and a variety of other helpful libraries.</p>
+      <Link to="/layout" activeClassName="active">Go to Layout</Link>
+    </div>
+  );
 }
 
 export default AboutPage;
