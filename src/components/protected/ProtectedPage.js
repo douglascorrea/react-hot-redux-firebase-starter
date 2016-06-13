2d26e2e4bc48eb8b1e954397987d0b3e384bcf60
jeblister <jeblister@gmail.com>
2016-06-13 20:32:25 +0000

    feature Protected routes : wrap protected ressources with HOC requireAuth
    change the feauture `Protected routes (needs to be logged in)` to use a Higher Order Components.
    
    This a alternative way using the redux store to decide whether to redirect the user to '/login'.

--- src/components/protected/ProtectedPage.js
+++ src/components/protected/ProtectedPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
-
+import checkAuth from '../requireAuth';
 const ProtectedPage = () => {
   return (
     <div>
@@ -10,4 +10,4 @@ const ProtectedPage = () => {
   );
 };
 
-export default ProtectedPage;
+export default checkAuth(ProtectedPage);
