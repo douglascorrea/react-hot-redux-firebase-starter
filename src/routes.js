87c64cf3462b1d7a6aa2508183fe3e099937fd09
Clément Lucas <clement.lucas@ynotek.fr>
2017-03-07 23:56:53 +0100

    routes : remove /protected and /about route + add /chat route
    Signed-off-by: Clément Lucas <clement.lucas@ynotek.fr>

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,11 @@ import {Route, IndexRoute} from 'react-router';
 import Layout from './components/Layout';
 import HomePage from './components/home/HomePage';
 import AdminPage from './components/admin/AdminPage';
-import ProtectedPage from './components/protected/ProtectedPage';
+import ChatRoomPage from './components/chat/ChatRoomPage';
 import AboutPage from './components/about/AboutPage';
 import LoginPage from './components/login/LoginPage'; //eslint-disable-line import/no-named-as-default
 import RegistrationPage from './components/registration/RegistrationPage'; //eslint-disable-line import/no-named-as-default
 import {requireAdmin} from './actions/authActions';
-
 
 export default function Routes(store) {
 
@@ -21,8 +20,7 @@ export default function Routes(store) {
     <Route path="/" component={Layout}>
       <IndexRoute component={HomePage}/>
       <Route path="layout" component={Layout}/>
-      <Route path="about" component={AboutPage}/>
-      <Route path="protected" component={ProtectedPage}/>
+      <Route path="chat" component={ChatRoomPage}/>
       <Route path="admin" component={AdminPage} onEnter={checkAdmin}/>
       <Route path="register" component={RegistrationPage}/>
       <Route path="login" component={LoginPage}/>
