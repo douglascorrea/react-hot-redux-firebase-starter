0a0f75baf7cf26dc81da91d03ea0d76aee05d0e9
Clément Lucas <clement.lucas@ynotek.fr>
2017-03-08 00:22:21 +0100

    components : Header styling + add chat link when user is logged in + remove admin and about links
    Signed-off-by: Clément Lucas <clement.lucas@ynotek.fr>

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,26 +1,18 @@
 import React, {PropTypes} from 'react';
 import {Link, IndexLink} from 'react-router';
-import LoadingDots from './LoadingDots';
-import LoginLink from './LoginLink';
+
 import LogoutLink from './LogoutLink';
-import AdminLink from './AdminLink';
+import LoginLink from './LoginLink';
 
-const Header = ({loading, signOut, auth, user}) => {
-
-  let loginLogoutLink = auth.isLogged ? <LogoutLink signOut={signOut} /> : <LoginLink />;
-  let adminLink = user.isAdmin ? <AdminLink /> : null;
+const Header = ({signOut, auth}) => {
+  const loginLogoutLink = auth.isLogged ? <LogoutLink signOut={signOut} /> : <LoginLink />;
+  const chatLink = auth.isLogged && <Link to="/chat" className="nav-item" activeClassName="active">Chat</Link>;
 
   return (
     <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
-      {" | "}
-      <Link to="/protected" activeClassName="active">Protected</Link>
-      {adminLink}
-      {" | "}
+      <IndexLink to="/" className="nav-item" activeClassName="active">Home</IndexLink>
+      {chatLink}
       {loginLogoutLink}
-      {loading && <LoadingDots interval={100} dots={20}/>}
     </nav>
   );
 };
@@ -29,7 +21,6 @@ Header.propTypes = {
   signOut: React.PropTypes.func.isRequired,
   auth: React.PropTypes.object.isRequired,
   user: React.PropTypes.object.isRequired,
-  loading: PropTypes.bool.isRequired
 };
 
 export default Header;
