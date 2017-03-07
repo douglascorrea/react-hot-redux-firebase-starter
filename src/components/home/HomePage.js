97ee6e019b44b5c70281ab5409182a5125a88e9d
Clément Lucas <clement.lucas@ynotek.fr>
2017-03-08 00:15:06 +0100

    components: remove About link in home page + inform ChatX version in home page
    Signed-off-by: Clément Lucas <clement.lucas@ynotek.fr>

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import {Link} from 'react-router';
 
 const HomePage = () => {
 	return (
 		<div className="jumbotron">
 			<h1>ChatX</h1>
-			<p>This is a prototype version of a chat.</p>
-			<Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+			<p>Version : 1.0</p>
 		</div>
 	);
 };
