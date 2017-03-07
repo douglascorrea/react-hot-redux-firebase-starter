4cc1bb67baec7f46edf6e7e024c9040644712d58
Clément Lucas <clement.lucas@ynotek.fr>
2017-03-08 00:21:26 +0100

    re-indentate file
    Signed-off-by: Clément Lucas <clement.lucas@ynotek.fr>

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,35 +1,37 @@
 /*eslint no-invalid-this: "error"*/
 /*eslint-env es6*/
-import React, { PropTypes, Component } from 'react';
+import React, {PropTypes, Component} from 'react';
 import {connect} from 'react-redux';
 import toastr from 'toastr';
 
-export default function (ComposedComponent){
+export default function (ComposedComponent) {
   class Authentication extends Component {
-    componentWillMount(){
-      if(!this.props.authenticated) {
+    componentWillMount() {
+      if (!this.props.authenticated) {
         this.context.router.push('/login');
         toastr.error('You need to be logged to access this page');
       }
     }
-    componentWillUpdate(nextProps){
-      if(!nextProps.authenticated) {
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
         this.context.router.push('/login');
         toastr.error('You need to be logged to access this page');
       }
     }
-    render(){
-              return <ComposedComponent {...this.props}/>;
-      }
+
+    render() {
+      return <ComposedComponent {...this.props}/>;
+    }
   }
   Authentication.contextTypes = {
-      router : PropTypes.object
-    };
-  Authentication.propTypes  = {
-    authenticated : PropTypes.bool
+    router: PropTypes.object
+  };
+  Authentication.propTypes = {
+    authenticated: PropTypes.bool
   };
   const mapStateToProps = (state) => ({
-    authenticated : state.auth.isLogged
+    authenticated: state.auth.isLogged
   });
   return connect(mapStateToProps)(Authentication);
 }
