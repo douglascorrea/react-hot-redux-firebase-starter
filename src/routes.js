1e67d5baba23c92b3abf32af2e94de15577a442d
Clément Lucas <clement.lucas@ynotek.fr>
2017-03-09 02:58:44 +0100

    routes : remove /chatrooms/:chatroomId route
    Signed-off-by: Clément Lucas <clement.lucas@ynotek.fr>

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,10 @@ import {Route, IndexRoute} from 'react-router';
 import Layout from './components/Layout';
 import HomePage from './components/home/HomePage';
 import AdminPage from './components/admin/AdminPage';
-import ChatRoomPage from './components/chat/ChatRoomPage';
-import AboutPage from './components/about/AboutPage';
+import ChatRoomListPage from './components/chat/ChatRoomListPage';
 import LoginPage from './components/login/LoginPage'; //eslint-disable-line import/no-named-as-default
 import RegistrationPage from './components/registration/RegistrationPage'; //eslint-disable-line import/no-named-as-default
-import {requireAdmin} from './actions/authActions';
+import {requireAdmin, requireAuth} from './actions/authActions';
 
 export default function Routes(store) {
 
@@ -16,11 +15,15 @@ export default function Routes(store) {
     store.dispatch(requireAdmin(nextState, replace, callback));
   };
 
+  const checkAuth = (nextState, replace) => {
+    store.dispatch(requireAuth(nextState, replace));
+  };
+
   return (
     <Route path="/" component={Layout}>
       <IndexRoute component={HomePage}/>
       <Route path="layout" component={Layout}/>
-      <Route path="chat" component={ChatRoomPage}/>
+      <Route path="chatrooms" component={ChatRoomListPage} onEnter={checkAuth}/>
       <Route path="admin" component={AdminPage} onEnter={checkAdmin}/>
       <Route path="register" component={RegistrationPage}/>
       <Route path="login" component={LoginPage}/>
