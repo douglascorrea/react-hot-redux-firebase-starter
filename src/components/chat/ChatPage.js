225f3b3e920d798d0279d4c4928e28e4025b17f5
bj0rge <bastien.jorge@gmail.com>
2017-04-02 17:38:58 +0200

    Rename: protected to chat

--- src/components/chat/ChatPage.js
+++ src/components/chat/ChatPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router';
 import checkAuth from '../requireAuth';
-const ProtectedPage = () => {
+const ChatPage = () => {
   return (
     <div>
-      <h1>You will only see this page if you are logged in</h1>
+      <h1>There is only this #chan, feel free to talk about</h1>
       <Link to="/" activeClassName="active">Home</Link>
     </div>
   );
 };
 
-export default checkAuth(ProtectedPage);
+export default checkAuth(ChatPage);
