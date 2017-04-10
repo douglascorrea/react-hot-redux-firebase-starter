004053bd63a0954502bd63cb036287b5aa599d70
gmp <georges@leaneous.com>
2017-04-10 10:44:12 +0200

    add chat page to router's config

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import ProtectedPage from './components/protected/ProtectedPage';
 import AboutPage from './components/about/AboutPage';
 import LoginPage from './components/login/LoginPage'; //eslint-disable-line import/no-named-as-default
 import RegistrationPage from './components/registration/RegistrationPage'; //eslint-disable-line import/no-named-as-default
+import ChatPage from './components/chat/ChatPage'; //eslint-disable-line import/no-named-as-default
 import {requireAdmin} from './actions/authActions';
 
 
@@ -26,6 +27,7 @@ export default function Routes(store) {
       <Route path="admin" component={AdminPage} onEnter={checkAdmin}/>
       <Route path="register" component={RegistrationPage}/>
       <Route path="login" component={LoginPage}/>
+      <Route path="chat" component={ChatPage}/>
     </Route>
   );
 }
