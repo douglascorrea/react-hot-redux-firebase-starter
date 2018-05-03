86af8e23b72433a3846294db188259c5a56b48c9
Guillaume ARM <trapcodien@gmail.com>
2018-05-03 16:31:57 +0200

    fix: do not spread props in <span>

--- src/components/common/LoadingDots.js
+++ src/components/common/LoadingDots.js
@@ -26,7 +26,7 @@ class LoadingDots extends React.Component {
       text += '.';
       dots--;
     }
-    return <span {...this.props}>{text}&nbsp;</span>;
+    return <span>{text}&nbsp;</span>;
   }
 }
 
