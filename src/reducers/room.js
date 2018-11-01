ae88df96fa1a9b0adb8e2ede464a22ca13ec221b
leon3s <lvernisse@student.42.fr>
2018-11-01 22:37:55 +0100

    [ BUGFIX ] reverse message data

--- src/reducers/room.js
+++ src/reducers/room.js
@@ -5,12 +5,13 @@ import initialState from './initialState';
 export default function authReducer(state = initialState.room, action) {
   switch (action.type) {
     case types.MESSAGE_LOAD_SUCCESS: {
-      let data = action.payload;
       const ids = state.messages.map(({id}) => id);
-      if (!state.messages.length) data = data.reverse();
       return Object.assign({}, state, {
         messages: ([
-          ...data.filter(({ id }) => !ids.includes(id)),
+          ...action.payload
+            .filter(({ id }) =>
+              !ids.includes(id)
+            ).reverse(),
           ...state.messages,
         ]),
       });
