5c36417e41285e1c737e8db9e37cbec3dacfa7e2
Alexis Busseneau <alexis@Bubus-Laptop.localdomain>
2019-08-13 14:47:26 +0200

    Add config for firebase

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // Firebase
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCNsG4Y4pkaVxqM809bpQeZ3wsFgVlCPcg",
-  authDomain: "react-hot-redux-firebase-start.firebaseapp.com",
-  databaseURL: "https://react-hot-redux-firebase-start.firebaseio.com",
-  storageBucket: "react-hot-redux-firebase-start.appspot.com"
+  apiKey: "AIzaSyBZaquVOTxLN0CE7nUeObek2w7uWCcZ3bA",
+  authDomain: "chat-room-a853a.firebaseapp.com",
+  databaseURL: "https://chat-room-a853a.firebaseio.com",
+  storageBucket: ""
 };
